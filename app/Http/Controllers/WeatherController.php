620dceb92294929ccf9ccd40cb972c2b02a9a19e
Bram van Osta <bramvanosta@gmail.com>
2017-11-06 13:41:37 +0100

    :sparkles: Adds DB caching support for WeatherForecast

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -2,26 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use Log;
+use App\WeatherForecast;
+
 class WeatherController extends Controller
 {
     public function getCurrentWeather()
     {
-        // Fetch database to check time of latest request
-        // If latest request is older than 1h -> fetch new data from API and store in database
-        // Else -> fetch data from database
-        $OPENWEATHERMAP_API_KEY = env('OPENWEATHERMAP_API_KEY');
+        $lastWeatherForecast = WeatherForecast::whereRaw('created_at >= NOW() - INTERVAL 1 HOUR')->first();
 
-        $url = 'https://api.openweathermap.org/data/2.5/weather?lat=44.992392&lon=0.598711&APPID='.$OPENWEATHERMAP_API_KEY.'&units=metric';
-        
-        $client = new \GuzzleHttp\Client();
-        $res = $client->request('GET', $url);
+        if ($lastWeatherForecast) {
+            Log::info('Using weather forecast from database');
 
-        $content = json_decode($res->getBody(), TRUE);
+            $response = array(
+                "temperature" => $lastWeatherForecast['temperature'],
+                "code" => $lastWeatherForecast['code']
+            );
+        } else {
+            Log::info('Fetching new weather forecast');
 
-        $response = array(
-            "temperature" => round($content["main"]["temp"]),
-            "code" => $content["weather"][0]["id"]
-        );
+            $OPENWEATHERMAP_API_KEY = env('OPENWEATHERMAP_API_KEY');
+            $url = 'https://api.openweathermap.org/data/2.5/weather?lat=44.992392&lon=0.598711&APPID='.$OPENWEATHERMAP_API_KEY.'&units=metric';
+            
+            $client = new \GuzzleHttp\Client();
+            $res = $client->request('GET', $url);
+    
+            $content = json_decode($res->getBody(), TRUE);
+    
+            $response = array(
+                "temperature" => round($content["main"]["temp"]),
+                "code" => $content["weather"][0]["id"]
+            );
+
+            WeatherForecast::updateOrCreate(
+                ['temperature' => $response['temperature']],
+                ['code' => $response['code']]
+            );
+        }
 
         return response()->json($response);
     }
